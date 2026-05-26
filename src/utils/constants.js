bfc2a7eeab7aa7ec704f8a1c303828955b064f20
Mohamad <mousamohamad370@gmail.com>
2026-05-26 14:37:38 +0300

    Setup scalable architecture and project structure

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const USER_ROLES = {
+  USER: 'user',
+  ADMIN: 'admin'
+}
+
+export const ORDER_STATUS = {
+  PENDING: 'pending',
+  PAID: 'paid',
+  PROCESSING: 'processing',
+  COMPLETED: 'completed',
+  REJECTED: 'rejected'
+}
+
+export const PAYMENT_STATUS = {
+  PENDING: 'pending',
+  VERIFIED: 'verified',
+  REJECTED: 'rejected'
+}
